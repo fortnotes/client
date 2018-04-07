835c2e3c6b39b9233165349bd4f49fcae5b072cf
Stanislav Kalashnik <darkpark.main@gmail.com>
2018-04-07 13:48:49 +0300

    add common external resources support in svg template

--- tasks/svg.js
+++ tasks/svg.js
@@ -9,29 +9,44 @@ var fs     = require('fs'),
     path   = require('path'),
     runner = require('node-runner'),
     tools  = require('node-runner/lib/tools'),
+    xmljs  = require('xml-js'),
     name   = 'svg',
     log    = runner.log.wrap(name);
 
 
+function importFile ( element, data ) {
+    // add content
+    element.elements = [{type: 'text', text: data.replace(/\s/g, '')}];
+
+    // clear
+    delete element.attributes.src;
+}
+
+
+function importSymbol ( element, data ) {
+    // extract content
+    data = xmljs.xml2js(data, {
+        compact: false,
+        trim: true,
+        nativeType: true,
+        ignoreComment: true,
+        ignoreDoctype: true,
+        ignoreDeclaration: true
+    }).elements[0];
+
+    // add content
+    element.attributes.id = 'svg-' + path.basename(element.attributes.src, '.svg');
+    element.attributes.viewBox = data.attributes.viewBox;
+    element.elements = data.elements;
+
+    // clear
+    delete element.attributes.src;
+}
+
+
 function build ( config, done ) {
-    var //util  = require('util'),
-        xmljs = require('xml-js'),
+    var //util = require('util'),
         root, data;
-
-    function load ( element ) {
-        var data = xmljs.xml2js(
-            fs.readFileSync(path.join(config.path, element.attributes.src)).toString(),
-            {compact: false, trim: true, nativeType: true, ignoreComment: true, ignoreDoctype: true, ignoreDeclaration: true}
-        ).elements[0];
-
-
-        element.attributes.id      = 'svg-' + path.basename(element.attributes.src, '.svg');
-        element.attributes.viewBox = data.attributes.viewBox;
-        element.elements = data.elements;
-
-        delete element.attributes.src;
-        //console.log(util.inspect(data, {colors: true, depth: 6}));
-    }
 
     data = xmljs.xml2js(
         fs.readFileSync(config.source).toString(),
@@ -42,17 +57,24 @@ function build ( config, done ) {
     //console.log(util.inspect(root, {colors: true, depth: 6}));
 
     root.elements.forEach(function ( element ) {
-        if ( element.name === 'style' ) {
-            element.elements[0].text = element.elements[0].text.replace(/\s/g, '');
-        }
-        // symbol without content
-        if ( element.name === 'symbol' && !element.elements && element.attributes.src ) {
-            load(element);
+        var data;
+
+        // external resource
+        if ( !element.elements && element.attributes.src ) {
+            // get content
+            data = fs.readFileSync(path.join(config.path, element.attributes.src)).toString();
+
+            if ( element.name === 'symbol' ) {
+                importSymbol(element, data);
+            } else {
+                importFile(element, data);
+            }
         }
     });
 
     // save generated result
     tools.write([{name: config.target, data: xmljs.js2xml(data, {spaces: config.spaces})}], log, done);
+    //console.log(util.inspect(root, {colors: true, depth: 6}));
 }
 
 
